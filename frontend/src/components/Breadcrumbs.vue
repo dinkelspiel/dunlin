44798f25c818dd0db3ad205166adacf35f41341f
dinkelspiel <mail@keii.dev>
2026-02-15 16:15:44 +0100

    feat: file breadcrumbs to dropdown

--- frontend/src/components/Breadcrumbs.vue
+++ frontend/src/components/Breadcrumbs.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="flex gap-4 items-center" v-for="(path, index) in pathSegments" :key="index">
-    <router-link :to="getPathLink(index)">
-      <div class="text-neutral-400">/</div>
-    </router-link>
-    <router-link :to="getPathLink(index)" class="flex gap-2 items-center cursor-pointer">
-      {{ path }}
-    </router-link>
-  </div>
+  <DropdownMenu v-if="pathSegments.length > 0">
+    <DropdownMenuTrigger
+      class="flex gap-2 items-center cursor-pointer text-neutral-400 hover:text-neutral-200 transition-colors"
+    >
+      <span class="text-neutral-400">/</span>
+      <span>{{ triggerLabel }}</span>
+      <ChevronDown class="size-4" />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="start">
+      <DropdownMenuItem
+        v-for="(path, index) in pathSegments"
+        :key="index"
+        class="cursor-pointer"
+        :style="{ paddingLeft: `${index * 12 + 8}px` }"
+        @click="navigateTo(index)"
+      >
+        <span class="text-neutral-400 mr-1">/</span>{{ path }}
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { ChevronDown } from 'lucide-vue-next'
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu'
 
 const props = defineProps<{
   filepath: string
@@ -18,10 +38,21 @@ const props = defineProps<{
   projectSlug: string
 }>()
 
+const router = useRouter()
+
 const pathSegments = computed(() => props.filepath.split('/').filter((e) => !!e))
+
+const triggerLabel = computed(() => {
+  const last = pathSegments.value[pathSegments.value.length - 1]
+  return last.length > 22 ? last.slice(0, 22) + '...' : last
+})
 
 const getPathLink = (index: number) => {
   const partialPath = pathSegments.value.slice(0, index + 1).join('/')
   return `/-/${props.teamSlug}/${props.projectSlug}/${partialPath}`
+}
+
+const navigateTo = (index: number) => {
+  router.push(getPathLink(index))
 }
 </script>
